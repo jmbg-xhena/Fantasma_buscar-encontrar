fe47abb2ea0a43e02379c7e5f4927e05bffd8d12
José María Beristain <josemaria_1998@outlook.com>
2021-01-29 17:04:06 -0600

    animacion de la trampa 1

--- fantasma_buscar_encontrar/Assets/Scripts/trampa.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/trampa.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class trampa : MonoBehaviour
 {
+    Animator anim;
+    Animation cerrar_clip;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("humano"))
+        {
+            anim.SetBool("cerrar", true);
+        }
     }
 }
