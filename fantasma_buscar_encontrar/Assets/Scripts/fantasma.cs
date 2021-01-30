8ab46ddd79fc49c3bee96735f7668d20992ca13d
Nato <49217848+Guaigon@users.noreply.github.com>
2021-01-30 05:31:57 -0600

    Tiling

--- fantasma_buscar_encontrar/Assets/Scripts/fantasma.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/fantasma.cs
@@ -26,7 +26,7 @@ public class fantasma : MonoBehaviour
         if (collision.transform.CompareTag("espectro"))
         {
             print("fantasma game over");
-            collision.gameObject.GetComponent<Animator>().SetTrigger("consuming");
+            collision.gameObject.GetComponent<Animator>().SetTrigger("Consuming");
         }
     }
 }
