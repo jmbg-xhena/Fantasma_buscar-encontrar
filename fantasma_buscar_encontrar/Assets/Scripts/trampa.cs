00a9e2902d46b055ba2208c0633b5d915f777307
Nato <49217848+Guaigon@users.noreply.github.com>
2021-01-31 02:14:43 -0600

    Trampa

--- fantasma_buscar_encontrar/Assets/Scripts/trampa.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/trampa.cs
@@ -6,6 +6,9 @@ public class trampa : MonoBehaviour
 {
     Animator anim;
     Animation cerrar_clip;
+
+    bool activated = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +22,31 @@ public class trampa : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.CompareTag("Amelia") || collision.transform.CompareTag("Aron"))
+        if(activated == false)
         {
-            gameObject.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
-            anim.SetBool("Activate", true);
-        }
-        if(collision.transform.CompareTag("Aron"))
-        {
-            anim.SetBool("Hit", true);
+            if (collision.transform.CompareTag("Amelia") || collision.transform.CompareTag("Aron"))
+            {
+                gameObject.GetComponent<SpriteRenderer>().maskInteraction = SpriteMaskInteraction.None;
+                anim.SetBool("Activate", true);
+                activated = true;
+            }
+            if (collision.transform.CompareTag("Aron"))
+            {
+                anim.SetBool("Hit", true);
+                //Mata a Aron
+            }
+            else if (collision.transform.CompareTag("Amelia"))
+            {
+                if (collision.gameObject.GetComponent<humano>().stick == true)
+                {
+                    collision.gameObject.GetComponent<humano>().stick = false;
+                }
+                else
+                {
+                    anim.SetBool("Hit", true);
+                    //mata a Amelia
+                }
+            }
         }
     }
 }
