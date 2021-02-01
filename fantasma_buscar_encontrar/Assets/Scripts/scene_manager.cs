361ddf60eae893a05d3d87047f488846b34dff78
José María Beristain <josemaria_1998@outlook.com>
2021-02-01 00:18:31 -0600

    camboa de nivel con goal

--- fantasma_buscar_encontrar/Assets/Scripts/scene_manager.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/scene_manager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class scene_manager : MonoBehaviour
 {
+    private bool aron_llego=false;
+    private bool amelia_llego=false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +17,24 @@ public class scene_manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (aron_llego && amelia_llego) {
+            nextScene();
+        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Aron")) {
+            aron_llego = true;
+        }
+
+        if (collision.transform.CompareTag("Amelia"))
+        {
+            amelia_llego = true;
+        }
+    }
+
     public void nextScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
