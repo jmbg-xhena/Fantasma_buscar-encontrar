875f196bc3ad043c802f06327e2ae8167f22030a
Nato <49217848+Guaigon@users.noreply.github.com>
2021-01-31 21:51:24 -0600

    Troncoterminado

--- fantasma_buscar_encontrar/Assets/Scripts/TumbarTronco.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/TumbarTronco.cs
@@ -5,23 +5,89 @@ using UnityEngine;
 public class TumbarTronco : MonoBehaviour
 {
 
-    public GameObject triggerIzquierdo;
-    public GameObject triggerDerecho;
-    public GameObject triggerArriba;
-    public GameObject triggerAbajo;
+    public DireccionTumbar triggerIzquierdo;
+    public DireccionTumbar triggerDerecho;
+    public DireccionTumbar triggerArriba;
+    public DireccionTumbar triggerAbajo;
+
     public GameObject troncoInferior;
     public GameObject troncoSuperior;
 
+    public Transform transformIzquierdo;
+    public Transform transformDerecho;
+    public Transform transformArriba;
+    public Transform transformAbajo;
 
+    public BoxCollider2D colliderAntes;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (triggerIzquierdo.miDireccion == true)
+        {
+            troncoSuperior.transform.position = transformDerecho.position;
+            troncoSuperior.transform.rotation = transformDerecho.rotation;
+
+            colliderAntes.gameObject.SetActive(false);
+
+            troncoSuperior.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+            troncoInferior.GetComponent<BoxCollider2D>().isTrigger = true;
+
+            triggerIzquierdo.gameObject.SetActive(false);
+            triggerDerecho.gameObject.SetActive(false);
+            triggerArriba.gameObject.SetActive(false);
+            triggerAbajo.gameObject.SetActive(false);
+        }
+        else if (triggerDerecho.miDireccion == true)
+        {
+            troncoSuperior.transform.position = transformIzquierdo.position;
+            troncoSuperior.transform.rotation = transformIzquierdo.rotation;
+
+            colliderAntes.gameObject.SetActive(false);
+
+            troncoSuperior.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+
+            troncoInferior.GetComponent<BoxCollider2D>().isTrigger = true;
+
+
+            triggerIzquierdo.gameObject.SetActive(false);
+            triggerDerecho.gameObject.SetActive(false);
+            triggerArriba.gameObject.SetActive(false);
+            triggerAbajo.gameObject.SetActive(false);
+        }
+        else if(triggerArriba.miDireccion == true)
+        {
+            troncoSuperior.transform.position = transformAbajo.position;
+            troncoSuperior.transform.rotation = transformAbajo.rotation;
+
+            colliderAntes.gameObject.SetActive(false);
+
+            troncoSuperior.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+            troncoInferior.GetComponent<BoxCollider2D>().isTrigger = true;
+
+            triggerIzquierdo.gameObject.SetActive(false);
+            triggerDerecho.gameObject.SetActive(false);
+            triggerArriba.gameObject.SetActive(false);
+            triggerAbajo.gameObject.SetActive(false);
+        }
+        else if(triggerAbajo.miDireccion == true)
+        {
+            troncoSuperior.transform.position = transformArriba.position;
+            troncoSuperior.transform.rotation = transformArriba.rotation;
+
+            colliderAntes.gameObject.SetActive(false);
+
+            troncoSuperior.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+            troncoInferior.GetComponent<BoxCollider2D>().isTrigger = true;
+
+            triggerIzquierdo.gameObject.SetActive(false);
+            triggerDerecho.gameObject.SetActive(false);
+            triggerArriba.gameObject.SetActive(false);
+            triggerAbajo.gameObject.SetActive(false);
+        }
     }
+
 }
