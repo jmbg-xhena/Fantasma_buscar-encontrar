c9ebe049e725130bbe6cb09d60015ab60ad5c093
Nato <49217848+Guaigon@users.noreply.github.com>
2021-01-31 20:17:57 -0600

    InicioTroncoCaida

---
+++ fantasma_buscar_encontrar/Assets/Scripts/TumbarTronco.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbarTronco : MonoBehaviour
+{
+
+    public GameObject triggerIzquierdo;
+    public GameObject triggerDerecho;
+    public GameObject triggerArriba;
+    public GameObject triggerAbajo;
+    public GameObject troncoInferior;
+    public GameObject troncoSuperior;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
