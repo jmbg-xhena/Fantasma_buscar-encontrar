7d442cd82f05a40556a6c25ad2952d48f7909657
José María Beristain <josemaria_1998@outlook.com>
2021-02-08 20:39:14 -0600

    bloquear posicion hitbox

--- fantasma_buscar_encontrar/Assets/Scripts/hitbox.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/hitbox.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class hitbox : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Vector3 init_pos;
     void Start()
     {
-        
+        init_pos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localPosition = init_pos;
     }
 }
