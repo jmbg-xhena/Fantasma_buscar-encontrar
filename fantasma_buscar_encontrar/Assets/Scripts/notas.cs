aca48b4043b35fbca5aaba50a5d577d9e97d7a7d
daxaer <48969078+daxaer@users.noreply.github.com>
2021-02-17 21:23:19 -0600

    notas

---
+++ fantasma_buscar_encontrar/Assets/Scripts/notas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class notas : MonoBehaviour
+{
+    public Image notaGrande;
+    private PlayerInput playerInput;
+
+
+    void Start()
+    {
+        playerInput = FindObjectOfType<PlayerInput>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Amelia"))
+        {
+            if (collision.GetComponent<humano>().agarrarObjeto == true)
+            {
+                collision.GetComponent<humano>().agarrarObjeto = false;
+                notaGrande.gameObject.SetActive(true);
+                playerInput.PauseGame();
+
+            }
+        }
+    }
+}
