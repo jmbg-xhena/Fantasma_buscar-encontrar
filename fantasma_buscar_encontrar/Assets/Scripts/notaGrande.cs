418d107f55d2d1c40bab095b13085c6f0cd4d973
Nato <49217848+Guaigon@users.noreply.github.com>
2021-02-21 20:40:40 -0600

    PausaCOnNotasFuncional

--- fantasma_buscar_encontrar/Assets/Scripts/notaGrande.cs
+++ fantasma_buscar_encontrar/Assets/Scripts/notaGrande.cs
@@ -6,26 +6,36 @@ public class notaGrande : MonoBehaviour
 {
     private PlayerInput playerInput;
     private bool puedoDesactivarme = false;
+    private bool primeraCourutina = false;
     // Start is called before the first frame update
     void Start()
     {
         playerInput = FindObjectOfType<PlayerInput>();
-        StartCoroutine(TiempoDeEspera());
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(primeraCourutina == false)
+        {
+            StartCoroutine(TiempoDeEspera());
+            primeraCourutina = true;
+        }
         if (playerInput.Input_interaccionP1.ReadValue<float>() == 1 && puedoDesactivarme == true)
         {
+            print("EntraASalir");
             playerInput.ResumeGame();
+            puedoDesactivarme = false;
+            primeraCourutina = false;
             this.gameObject.SetActive(false);
         }
     }
 
     IEnumerator TiempoDeEspera()
     {
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSecondsRealtime(.5f);
+        print("Puede salir");
         puedoDesactivarme = true;
     }
 }
